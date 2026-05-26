aa2d2906d5b6f46b591030500578ef835f834324
divijix <divijixtechnology@zohomail.in>
2026-05-26 23:06:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-import app from './src/app.js'
+// import app from './src/app.js'
 
 
-app.listen(3000,()=>{console.log("app started at port 3000")});
+// app.listen(3000,()=>{console.log("app started at port 3000")});
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+export default app;
