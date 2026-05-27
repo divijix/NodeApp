91f5c0f775b9ef4ffc4b22beac86c2fe202aa05d
0xReflex <prrprrbro23@gmail.com>
2026-05-27 11:49:55 +0530

    made some changes

--- NodeApp-main/server.js
+++
@@ -1,4 +0,0 @@
-import app from './src/app.js'
-
-
-app.listen(3000,()=>{console.log("app started at port 3000")});
--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-// import app from './src/app.js'
+import app from './src/app.js'
 
 
-// app.listen(3000,()=>{console.log("app started at port 3000")});
-
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Server running");
-});
-
-export default app;
+app.listen(3000,()=>{console.log("app started at port 3000")});
