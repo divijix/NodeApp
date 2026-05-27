b0f3c6f6661ffd4def35f3c58342ab8c80647796
jitendra shukla <69817757+jit9628@users.noreply.github.com>
2026-05-27 12:14:13 +0530

    Remove duplicate nested NodeApp-main folder

--- NodeApp-main/NodeApp-main/README.md
+++
@@ -1,3 +0,0 @@
-# the work still to do
-1. json error handling
-2. types of tokens to implement [refresh/access tokens]
--- NodeApp-main/server.js
+++
@@ -1,9 +0,0 @@
-import app from './src/app.js'
-
-app.get("/", (req, res) => {
-  res.send("Server running");
-});
-
-app.listen(3000,()=>{console.log("app started at port 3000")});
-
-export default app;
