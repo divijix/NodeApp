6095a7c5cc6c6a7768c3d14d559bde43851b990b
0xReflex <prrprrbro23@gmail.com>
2026-05-28 19:33:44 +0530

    Added changes for the role based authentication and only admin can post/get from some endpoints

--- src/middleware/verifyJwt.js
+++ src/middleware/verifyJwt.js
@@ -22,3 +22,27 @@ export async function verifyUser(req, res, next){
     }
 
 }
+
+export function verifyAdmin(req,res,next){
+    const cookie = req.cookies?.uuid;
+
+    console.log(cookie);
+
+    if(!cookie){
+        return res.status(401).json({message:"Unauthorized"});
+    }
+
+    try{
+        const verified = jwt.verify(cookie, config.JWT_KEY);
+        if(verified.role !== 'admin'){
+            return res.status(401).json({message:"Unauthorized"})
+        }
+        // passing the verified data to the req for the next middlewares
+        req.user = verified;
+        
+        next();
+    }catch(err){
+        return res.status(403).json({message:"Invalid"});
+    }
+
+}
